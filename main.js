262fbe26734cf75d7a6869b979854c5da026ffb5
Swenson <swensonmbailey@yahoo.com>
2022-04-21 16:23:44 -0500

    main.js and index.html

---
+++ main.js
@@ -0,0 +1,14 @@
+const links = [
+    { 
+      label: "Week1",
+      url: "week1/index.html",
+    }
+  ]
+
+function create_List(){
+    links.forEach(create_List_Item)
+}
+function create_List_Item(item){
+    let theList = document.getElementById("the_list").innerHTML;
+    theList += '<li> <a href="' + item.url + '"> ' + item.label + '</a> </li>'
+}
