28b8dad8b516de215c681a42509404d73594a656
Swenson <swensonmbailey@yahoo.com>
2022-04-21 20:53:28 -0500

    changes

--- week1/main.js
+++ week1/main.js
@@ -7,13 +7,14 @@ const links = [
         label: "Week1 notes",
         url: "notes.txt"
     }
-  ]
+]
+var theList = "";
 
 function create_List(){
     links.forEach(create_List_Item)
 }
 function create_List_Item(item){
-    let theList = "";
+    
     theList += '<li> <a href="' + item.url + '"> ' + item.label + '</a> </li>'
     document.getElementById("the_list").innerHTML = theList;
 }
