b1437d5e13d23ba0d221e1234b5b3947bd1f4c47
Swenson <swensonmbailey@yahoo.com>
2022-04-25 20:26:00 -0500

    set up week2

---
+++ week2/main.js
@@ -0,0 +1,16 @@
+const links = [
+    {
+        label: "Week2 notes",
+        url: "notes.txt"
+    }
+]
+var theList = "";
+
+function create_List(){
+    links.forEach(create_List_Item)
+}
+function create_List_Item(item){
+    
+    theList += '<li> <a href="' + item.url + '"> ' + item.label + '</a> </li>'
+    document.getElementById("the_list").innerHTML = theList;
+}
