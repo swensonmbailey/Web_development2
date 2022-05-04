d2ae1fa9ad2490031920688d9fd63ce89e89d1a4
Swenson <swensonmbailey@gmail.com>
2022-05-03 21:14:33 -0500

    sets up week 3

--- week3/main.js
+++ week3/main.js
@@ -1,23 +1,17 @@
 const links = [
-    { 
-      label: "Week1",
-      url: "week1/index.html",
-    },
     {
-      label: "Week2",
-      url: "week2/index.html"
+        label: "Week3 notes",
+        url: "notes.txt"
     },
-    {
-      label: "Week3",
-      url: "week3/index.html"
-    }
-  ]
+    
+]
+var theList = "";
 
 function create_List(){
     links.forEach(create_List_Item)
 }
 function create_List_Item(item){
-    let theList = document.getElementById("the_list").innerHTML;
+    
     theList += '<li> <a href="' + item.url + '"> ' + item.label + '</a> </li>'
     document.getElementById("the_list").innerHTML = theList;
 }
