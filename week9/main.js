7dba4bc2de4dca23e7002f61a5a84d5ce4f67c59
Swenson <swensonmbailey@gmail.com>
2022-06-13 20:44:35 -0500

    sets up week 9

---
+++ week9/main.js
@@ -0,0 +1,21 @@
+const links = [
+    {
+        label: "Week9 notes",
+        url: "notes.txt"
+    },
+    {
+        label: "Week8 team activity",
+        url: "../team-activity-week8/index.html"
+    }
+    
+]
+var theList = "";
+
+function create_List(){
+    links.forEach(create_List_Item)
+}
+function create_List_Item(item){
+    
+    theList += '<li> <a href="' + item.url + '"> ' + item.label + '</a> </li>'
+    document.getElementById("the_list").innerHTML = theList;
+}
