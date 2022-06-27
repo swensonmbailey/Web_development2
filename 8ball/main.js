aa3624b59e79f3682fe8bb8d121a9b75f09235bb
Swenson <swensonmbailey@gmail.com>
2022-06-26 22:28:53 -0500

    css changes

---
+++ 8ball/main.js
@@ -0,0 +1,7 @@
+// document.getElementById('ask').addEventListener('click', (e)=>{
+//     e.preventDefault();
+//     let r = document.querySelector(':root');
+//     r.style.setProperty('--repetition', 'infinite');
+
+// }, false);
+
