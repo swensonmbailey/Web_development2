a19f3ffb514b599feeb7671328abfbf188cabda8
Swenson <swensonmbailey@gmail.com>
2022-06-30 20:28:48 -0500

    adds create account

--- 8ball/main.js
+++ 8ball/main.js
@@ -10,3 +10,15 @@ document.getElementById('login').addEventListener('click', (e)=>{
     document.getElementById("pageContents").style.display = "none";
 
 }, false);
+
+document.getElementById('back').addEventListener('click', (e)=>{
+    document.getElementById("loginContents").style.display = "none";
+    document.getElementById("pageContents").style.display = "flex";
+
+}, false);
+
+document.getElementById('createAcc').addEventListener('click', (e)=>{
+    document.getElementById("createAccDiv").style.display = "block";
+    document.getElementById("loginH1").innerHTML = "8-Ball Create Account"
+    document.getElementById("loginP").innerHTML = "Have an account? <br> <span id='createAcc'>Sign in</span>"
+}, false);
